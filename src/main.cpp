92885fea37b75abab785093203c70a9bee3dc330
Corneliu Nastas <cnastas@genevamedia.com>
2024-03-02 20:05:11 +0200

    feat: test commit.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 #include <Arduino.h>
 
+#include "Button.hpp"
+
+#define RED_BUTTON_PIN 5
+
+void myFunction() {
+  Serial.println("Clicked Red !");
+}
+
+Button redButton(RED_BUTTON_PIN, myFunction);
+
 void setup() {
+  Serial.begin(9600);
   Serial.println("Test Serial");
 }
 
 void loop() {
+  redButton.checkForClick();
 }
